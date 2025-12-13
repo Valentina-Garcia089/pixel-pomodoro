5b93c7d06b50b9ba7bc0bea3e0cd7bde86ae588e
Valentina Garcia <valentinagarciag089@gmail.com>
2025-12-13 01:00:46 -0500

    First modifications for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // se crearán estados para el modo en el que se encuentra el pomodoro, para el tiempo faltante y para saber si este está corriendo
+  const [mode, setMode] = useState("focus");
+  const [timeLeft, setTimeLeft] = useState(1500);
+  const [running, setRunning] = useState(false);
+
+  function handleStart (){
+    setRunning(true);
+  }
+
+  function handlePause (){
+    setRunning(false);
+  }
+
+  function handleReset (){
+    setRunning(false);
+  }
+
+  useEffect(() => {
+    let interval;
+    if(running){
+      interval = setInterval(() =>{
+        setTimeLeft(prev => prev - 1);
+      })
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [running]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div> modo: {mode}</div>
+      <div> timeLeft: {timeLeft}</div>
+      <div> running: {running === true ? "true" : "false"}</div>
+
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handlePause}>Pause</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   )
 }
